272b53014ab7b8d850b14acefb2a3d2c8cf6be92
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2023-08-08 17:01:53 +0300

    Get Enumerator with yield is added to Linked List

--- Viktor/Nemetschek_Exercise/Stack_and_Queue_Implementation/Stack_and_Queue_Implementation/CustomNonBinaryTree.cs
+++ Viktor/Nemetschek_Exercise/Stack_and_Queue_Implementation/Stack_and_Queue_Implementation/CustomNonBinaryTree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,13 @@ namespace VM.DataStructures
             Value = v;
         }
     }
-    public class TreeNode<T> : Leaf<T>
+    public class TreeNode<T> : Leaf<T>, IEnumerator<T>
     {
         private List<Leaf<T>> children;
 
+        public T Current => throw new NotImplementedException();
 
+        object IEnumerator.Current => throw new NotImplementedException();
 
         public TreeNode(T value) : base(value)
         {
@@ -77,66 +80,20 @@ namespace VM.DataStructures
 
             return null;
         }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
-    /*public class CustomNonBinaryTree<T>
-    {
-        public int Count { get; private set; }
-        private TreeNode<T> root;
-        public CustomNonBinaryTree()
-        {
-            root = null;
-        }
-        public void AddRoot(T value)
-        {
-            if (root == null)
-            {
-                root = new TreeNode<T>(value);
-                Count++;
-            }
-            else
-            {
-                throw new InvalidOperationException("There is root node already in the tree.");
-            }
-        }
-
-        public void AddChild(TreeNode<T> parent, T value)
-        {
-            if (parent == null)
-            {
-                throw new ArgumentNullException(nameof(parent));
-            }
-            if (parent.firstChild == null)
-            {
-                parent.firstChild = new TreeNode<T>(value);
-            }
-            else
-            {
-                var currentSibling = parent.firstChild;
-                while (currentSibling.nextSibling != null)
-                {
-                    currentSibling = currentSibling.nextSibling;
-                }
-                currentSibling.nextSibling = new TreeNode<T>(value);
-            }
-            Count++;
-        }
-        public void RemoveElement(T value)
-        {
-            if (root.value.Equals(value))
-            {
-                root = null;
-                Count = 0;
-                return;
-            }
-            var currentChild
-            if (root.firstChild.value.Equals(value))
-            {
-                root.firstChild = root.firstChild.nextSibling;
-                Count--;
-                return;
-            }
-
-        }
-            
-    }*/
 }
