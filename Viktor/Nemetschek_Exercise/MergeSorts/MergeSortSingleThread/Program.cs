96e29b297361ba0935f6067d65a9fb4dd1d2fb4e
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2023-08-14 16:06:14 +0300

    Merge method using single thread is done

---
+++ Viktor/Nemetschek_Exercise/MergeSorts/MergeSortSingleThread/Program.cs
@@ -0,0 +1,68 @@
+﻿int[] arr = { 8, 4, 2, 6, 10, 1, 3, 5, 7, 9 };
+
+MergeSort(arr, 0, arr.Length - 1);
+
+foreach (var num in arr)
+{
+    Console.Write(num + " ");
+}
+
+static void MergeSort(int[] arr, int left, int right)
+{
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
+
+        MergeSort(arr, left, mid);
+        MergeSort(arr, mid + 1, right);
+
+        Merge(arr, left, mid, right);
+    }
+}
+
+static void Merge(int[] arr, int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int[] leftArray = new int[n1];
+    int[] rightArray = new int[n2];
+
+    for (int i = 0; i < n1; i++)
+        leftArray[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        rightArray[j] = arr[mid + 1 + j];
+
+    int k = left;
+    int l = 0, r = 0;
+
+    while (l < n1 && r < n2)
+    {
+        if (leftArray[l] <= rightArray[r])
+        {
+            arr[k] = leftArray[l];
+            l++;
+        }
+        else
+        {
+            arr[k] = rightArray[r];
+            r++;
+        }
+        k++;
+    }
+
+    while (l < n1)
+    {
+        arr[k] = leftArray[l];
+        l++;
+        k++;
+    }
+
+    while (r < n2)
+    {
+        arr[k] = rightArray[r];
+        r++;
+        k++;
+    }
+}
+
