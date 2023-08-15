49cab947bf164f35da1d62d363fee9a03dc46a56
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2023-08-15 11:20:18 +0300

    example of implementation of priority queue

---
+++ Hristo/PriorityQueue/PriorityQueue/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        SortedSet<int> priorityQueue = new()
+        {
+            6,
+            5,
+            2,
+            8,
+            3,
+            4,
+            1,
+            10,
+            7,
+            9
+        };
+
+        priorityQueue.Remove(priorityQueue.Max);
+        priorityQueue.Remove(priorityQueue.Min);
+        priorityQueue.Reverse();
+
+        foreach (int element in priorityQueue)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
