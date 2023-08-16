cc07fff0c7f8446b3209850bac6b3b1ee9b8839a
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2023-08-16 16:07:05 +0300

    chat

---
+++ Hristo/ChatRoom/ChatRoom/ChatRoom.cs
@@ -0,0 +1,18 @@
+﻿namespace ChatRoom
+{
+    public class ChatRoom
+    {
+        public event EventHandler<ChatMessagesEventArgs> MessageSend;
+        public List<string> Participants { get; set; }    
+        public string  Name { get; private set; }
+        public ChatRoom(string name)
+        {
+            Name = name;
+        }
+        public void PublishMessage(string message, string username)
+        {
+            var messageArgs = new ChatMessagesEventArgs(message, username);
+            this.MessageSend(this, messageArgs);
+        }
+    }
+}
